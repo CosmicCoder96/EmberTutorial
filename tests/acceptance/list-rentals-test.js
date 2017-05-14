4e93e6d855fd053992e4320955ce948f108006cb
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-05-14 15:51:55 +0530

    Test added to check redirection from index to rentals page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,6 +4,11 @@ import moduleForAcceptance from 'tutorial/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should show rentals as the home page', function (assert) {
+  visit('/');
+    andThen(function() {
+      assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+    })
+
 });
 
 test('should link to information about the company.', function (assert) {
